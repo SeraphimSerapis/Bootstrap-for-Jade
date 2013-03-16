54b745b11bc143966855b0142fe52c9040f3ddc1
JoeChapman <joseph.philip.chapman@gmail.com>
2013-03-16 21:24:12 +0000

    Bumped express version to 3.1.0 and updated app.js express code.

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
-
 /**
  * Module dependencies.
  */
 
 var express = require('express')
-  , routes = require('./routes');
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
 
-var app = module.exports = express.createServer();
+var app = express();
 
 // Configuration
 
 app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser());
   app.use(express.session({ secret: 'your secret here' }));
   app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+  app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
@@ -35,6 +40,7 @@ app.get('/', routes.index);
 app.get('/home', routes.home);
 app.get('/about', routes.about);
 
-app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
 });
+
